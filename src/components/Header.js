c9ad7890ef0739d6b89f09fcee24cbb39274fbe2
Vova Pol <ya-chai777@yandex.ru>
2022-11-07 12:26:10 +0300

    refactor: Создал компоненты Header, Main, Footer

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+      <hr className="header__line" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+function Main() {
+  return (
+    <main>
+      <section className="profile section-sizing">
+        <img src="#" alt="аватар профайла" className="profile__picture" />
+        <div className="profile__edit-icon-container">
+          <span className="profile__edit-icon"></span>
+        </div>
+        <article className="profile__info">
+          <div className="profile__container">
+            <h1 className="profile__name">Жак Кусто</h1>
+            <p className="profile__occupation">Исследователь океана</p>
+          </div>
+          <button className="profile__edit-button"></button>
+        </article>
+        <button className="profile__add-button"></button>
+      </section>
+
+      <section className="elements section-sizing">
+        <ul className="elements__list">
+          <template id="place-card">
+            <li className="elements__item">
+              <img src="#" alt="" className="elements__image" />
+              <div className="elements__info">
+                <h2 className="elements__title"></h2>
+                <div className="elements__like-container">
+                  <button className="elements__like-button"></button>
+                  <span className="elements__like-counter"></span>
+                </div>
+              </div>
+              <button className="elements__delete-button"></button>
+            </li>
+          </template>
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
