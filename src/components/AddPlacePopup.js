7babd968f71f72fd2925b0b281850f64b26eed07
Vova Pol <ya-chai777@yandex.ru>
2022-11-29 13:34:05 +0300

    refactor: переписать компонент AddPlacePopup, используя управляемый компонент вместо рефа

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -2,19 +2,27 @@ import React from "react";
 import PopupWithForm from "./PopupWithForm";
 
 function AddPlacePopup(props) {
-  const inputNameRef = React.useRef();
-  const inputLinkRef = React.useRef();
+  const [inputName, setInputName] = React.useState("");
+  const [inputLink, setInputLink] = React.useState("");
+
+  function handleChangeInputName(evt) {
+    setInputName(evt.target.value);
+  }
+
+  function handleChangeInputLink(evt) {
+    setInputLink(evt.target.value);
+  }
 
   function handleSubmit(evt) {
     evt.preventDefault();
 
     props.onUpdateCards({
-      name: inputNameRef.current.value,
-      link: inputLinkRef.current.value,
+      name: inputName,
+      link: inputLink,
     });
 
-    inputNameRef.current.value = "";
-    inputLinkRef.current.value = "";
+    setInputName("");
+    setInputLink("");
   }
 
   return (
@@ -35,7 +43,7 @@ function AddPlacePopup(props) {
         required
         minLength="2"
         maxLength="30"
-        ref={inputNameRef}
+        onChange={handleChangeInputName}
       />
       <span className="popup__input-error" id="place-name-input-error"></span>
       <input
@@ -45,7 +53,7 @@ function AddPlacePopup(props) {
         className="popup__input"
         id="place-link-input"
         required
-        ref={inputLinkRef}
+        onChange={handleChangeInputLink}
       />
       <span className="popup__input-error" id="place-link-input-error"></span>
     </PopupWithForm>
